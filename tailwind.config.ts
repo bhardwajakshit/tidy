5b3a66b327cf032a878af79b82b9c5ee1ece245d
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-10-29 15:45:23 +0530

    chore: added colors in tailwind config, and updated fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "tidy-white": "#f8f7f1",
+        "tidy-black": "#222222",
+      },
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+        mono: ["Fira Mono", "monospace"],
       },
     },
   },
