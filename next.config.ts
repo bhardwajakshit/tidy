ae55f3c00e8591c5274d1bdc796dcd322c7b1187
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-11-04 20:29:47 +0530

    chore: setup for cloudflare pages

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -6,5 +7,9 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: false,
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
