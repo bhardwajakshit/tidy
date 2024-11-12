6d80cbacfff0840cf3d7178dd5b22d4111ec883e
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-11-12 18:12:49 +0530

    chore: added auth middleware and provider

---
+++ src/utils/providers/auth-provider.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { createBrowserClient } from "@supabase/ssr";
+import { useRouter } from "next/navigation";
+import { User } from "@prisma/client";
+import axios from "axios";
+import { Loader } from "@/components/common/Loader";
+
+type SupabaseContextType = {
+  user: User | null;
+  loading: boolean;
+  signInWithGoogle: () => Promise<void>;
+  signOut: () => Promise<void>;
+};
+
+const SupabaseContext = createContext<SupabaseContextType | undefined>(
+  undefined
+);
+
+export function SupabaseProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter();
+
+  console.log("user in shared state: ", user)
+
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );    
+
+  useEffect(() => {
+    console.log("auth prov use effect ran!");
+    const fetchUser = async () => {
+      try {
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+
+        if (user) {
+          console.log("session user is available, fetching user data from db");
+          try {
+            const response = await axios.get("/api/user");
+            const userData = await response.data;
+            setUser(userData);
+          } catch (error) {
+            console.error("Error fetching user:", error);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching session user:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  const signInWithGoogle = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/api/auth/callback`,
+      },
+    });
+    if (error) throw error;
+  };
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    router.push("/");
+  };
+
+  if(loading) return <Loader/>
+
+  return (
+    <SupabaseContext.Provider
+      value={{
+        user,
+        loading,
+        signInWithGoogle,
+        signOut,
+      }}
+    >
+      {children}
+    </SupabaseContext.Provider>
+  );
+}
+
+export const useSupabase = () => {
+  const context = useContext(SupabaseContext);
+  if (context === undefined) {
+    throw new Error("useSupabase must be used inside SupabaseProvider");
+  }
+  return context;
+};
