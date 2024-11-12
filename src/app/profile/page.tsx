16ba2c00329320663931756a1966cb3a67a37683
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-11-13 00:34:12 +0530

    chore: added profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Header } from "@/components/common/Header";
+import { useSupabase } from "@/utils/providers/auth-provider";
+import Image from "next/image";
+
+export default function Profile() {
+  const { user } = useSupabase();
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-between">
+      <Header />
+
+      <div>
+        <div className="flex justify-center mb-6">
+          <Image
+            src={user?.image || ""}
+            alt="Profile Picture"
+            width={80}
+            height={80}
+            className="rounded-full border-2 border-gray-200"
+          />
+        </div>
+
+        <div className="text-center mb-6">
+          <h1 className="text-3xl font-medium text-gray-800">{user?.name}</h1>
+          <p className="text-sm text-gray-500">{user?.email}</p>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
