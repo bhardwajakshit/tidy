06abce712d532878d956c9b38912d9c99aba8640
bhardwajakshit <bhardwajakshit78@gmail.com>
2024-11-26 13:42:00 +0530

    chore: updated page for google oauth verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import TypewriterText from '@/components/common/StaggerText';
 import { motion } from 'framer-motion';
 import { FcGoogle } from 'react-icons/fc';
 import { useSupabase } from '@/utils/providers/auth-provider';
 import { LandingFooter } from '@/components/common/LandingFooter';
+import TypewriterText from '@/components/common/TypewriterText';
 
 export default function Auth() {
   const { signInWithGoogle } = useSupabase();
@@ -13,8 +13,18 @@ export default function Auth() {
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="h-10" />
 
-      <div className="flex flex-grow flex-col items-center justify-center">
+      <div className="flex flex-grow flex-col items-center justify-center gap-4">
         <TypewriterText text="tidy." className="text-3xl font-semibold" />
+        <motion.p
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: 'easeOut', delay: 3.2 }}
+          className="max-w-lg text-center text-base text-tidy-black/60"
+        >
+          Tidy is a simple and intuitive app designed to help you stay organized
+          and productive. Track your tasks, set goals, and focus on making
+          consistent progress with ease.
+        </motion.p>
       </div>
 
       <motion.button
