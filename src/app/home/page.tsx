4c2d5c5eedfd6848d6036356efc42884a3f3ba87
bhardwajakshit <bhardwajakshit78@gmail.com>
2025-04-25 00:54:37 +0530

    feat: update home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,46 @@
 'use client';
 
-import { RefObject, useEffect, useRef, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Header } from '@/components/common/Header';
 import { Footer } from '@/components/common/Footer';
 import { CreateTaskModal } from '@/components/task/CreateTaskModal';
 import { TaskCard } from '@/components/common/TaskCard';
-import { devGoalsData } from '@/utils/constants';
+import axios from 'axios';
+import { Loader } from '@/components/common/Loader';
+
+export type Task = {
+  id: string;
+  title: string;
+  description: string;
+  priority: string;
+  notes: string[];
+  cardColor: string;
+  textColor: string;
+  createdAt: Date;
+};
 
 export default function Dashboard() {
-  const [tasks, setTasks] = useState(devGoalsData);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const ref = useRef(null) as RefObject<HTMLDivElement>;
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        setIsFetching(true);
+        const response = await axios.get('/api/tasks');
+        const data = await response.data;
+        setTasks(data);
+      } catch (error) {
+        setIsFetching(false);
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -38,20 +68,24 @@ export default function Dashboard() {
     >
       <Header />
 
-      {tasks.map((goal) => (
-        <TaskCard
-          key={goal.id}
-          bgColor={goal.bgColor}
-          textColor={goal.textColor}
-          title={goal.title}
-          description={goal.description}
-          priority={goal.priority}
-          date={goal.date}
-          ref={ref}
-        />
-      ))}
+      {!isFetching && tasks.length === 0 ? (
+        <div className="flex h-screen items-center justify-center">
+          <h1 className="text-xl font-bold">No tasks yet.</h1>
+        </div>
+      ) : tasks.length > 0 ? (
+        tasks.map((task) => (
+          <TaskCard key={task.id} ref={ref} task={task} setTasks={setTasks} />
+        ))
+      ) : (
+        <Loader />
+      )}
 
-      {isModalOpen && <CreateTaskModal onClose={() => setIsModalOpen(false)} />}
+      {isModalOpen && (
+        <CreateTaskModal
+          onClose={() => setIsModalOpen(false)}
+          updateTasks={setTasks}
+        />
+      )}
 
       {!isModalOpen && <Footer />}
     </div>
