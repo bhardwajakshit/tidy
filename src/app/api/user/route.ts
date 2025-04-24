43dcebdc4f76bbd3ad4e196c34b287465df97e2f
bhardwajakshit <bhardwajakshit78@gmail.com>
2025-04-25 00:51:26 +0530

    feat: add user deletion endpoint

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,7 @@
 import { getOrCreateUser } from '@/utils/auth';
 import { createClient } from '@/utils/supabase/server';
 import { NextResponse } from 'next/server';
+import { prisma } from '../../../../prisma/prisma';
 
 export async function GET() {
   const supabase = await createClient();
@@ -22,7 +23,44 @@ export async function GET() {
 
     return NextResponse.json(newUser);
   } catch (error) {
-    console.error('Error in user route:', error);
-    return new NextResponse('Internal Server Error', { status: 500 });
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE() {
+  const supabase = await createClient();
+
+  try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    await prisma.task.deleteMany({
+      where: {
+        userEmail: user.email,
+      },
+    });
+
+    await prisma.user.delete({
+      where: {
+        email: user.email,
+      },
+    });
+
+    return NextResponse.json({
+      message: 'User and related tasks deleted successfully.',
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
   }
 }
